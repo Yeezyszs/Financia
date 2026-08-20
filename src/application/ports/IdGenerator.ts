3b2f96f485ca20be2444e4e394c0cf4116c316bc
Pedro Maraia <pedromaraia454@gmail.com>
2026-08-20 10:03:04 -0300

    Delete src directory

--- src/application/ports/IdGenerator.ts
+++
@@ -1,8 +0,0 @@
-/** Injetado para os testes conseguirem ids previsiveis. */
-export interface IdGenerator {
-  next(): string;
-}
-
-export const cryptoIdGenerator: IdGenerator = {
-  next: () => crypto.randomUUID(),
-};
